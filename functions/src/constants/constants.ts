61de114a38617af680171e4674e8b05590e45d8d
himanshu saraswat <himanshusaraswat@outlook.com>
2020-04-04 18:12:42 +0530

    Cloud Function integrated with SendGrid to send transactional emails quickly

---
+++ functions/src/constants/constants.ts
@@ -0,0 +1,3 @@
+export const onlyAlpha = /^[A-Za-z ]+$/;
+export const emailPattern =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const from = { from: 'himanshu.s@hotmail.com' }
---
+++ functions/src/index.ts
@@ -0,0 +1,69 @@
+// Firebase Config
+import * as functions from 'firebase-functions';
+// Sendgrid Config
+import * as sgMail from '@sendgrid/mail';
+import { onlyAlpha, emailPattern, from } from './constants/constants';
+
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+
+sgMail.setApiKey(API_KEY);
+
+// Owner Account details
+const ACCOUNT_DETAILS = {
+    ...from,
+    templateId: TEMPLATE_ID,
+}
+
+const validateEmail=(value:any, pattern:string):boolean => {       
+    const receivedValue = value.toLowerCase().trim();
+    
+    switch (pattern) {
+        case 'email':
+            return emailPattern.test(String(receivedValue));
+        case 'alpha':
+            return onlyAlpha.test(String(receivedValue));
+        default:
+            return false;
+    }
+}
+// Sends email via HTTP. Can be called from frontend code. 
+exports.cffSendgridEmail = functions.https.onCall(async (data, context:any) => {
+    try {
+        
+        if (!context.auth && !context.auth.token.email) {
+            throw new functions.https.HttpsError('failed-precondition', 'Must be logged with an email address');
+        }
+        if(!data.delegateEmailDetails || !data.delegateEmailDetails.dynamic_template_data){
+            throw new functions.https.HttpsError('invalid-argument', `Root keys are missing`);
+        }else{
+            Object.entries(data.delegateEmailDetails).forEach(([key, value])=>{
+                if(key==='to'){
+                    if(!validateEmail(value,'email')){
+                        throw new functions.https.HttpsError('invalid-argument', `Value validation failed`);
+                    }
+                }
+                if(key==='dynamic_template_data'){
+                    Object.entries(data.delegateEmailDetails.dynamic_template_data).forEach(([template_key, template_value])=>{
+                        if(!validateEmail(template_value,'alpha')){
+                            throw new functions.https.HttpsError('invalid-argument', `dynamic_template_data value validation failed`);
+                        };
+                    });
+                }
+            });
+        }
+        
+        const msg = {...ACCOUNT_DETAILS, ...data.delegateEmailDetails};
+        await sgMail.send(msg);
+        console.log(`Email sent successfully ${JSON.stringify(data)}, by logged in user ${context.auth.token.email}:`);
+        return {
+            success: true
+        };
+    }
+    catch (error) {
+        console.log(`${error}, occurred while executing for ${JSON.stringify(data)}, sent by logged in user ${context.auth.token.email}:`);
+        return {
+            success: false
+        };
+    }
+});
